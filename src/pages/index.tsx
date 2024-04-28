dbcea77fee9cab1a373ba725dec221787ddfb17c
codeur-974 <eric974.sermande@gmail.com>
2024-04-28 10:39:09 +0400

    premier Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from "@/ui/components/seo";
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="Monaco974" description="Site pour les suporters de Monaco" />
+      <h1 className="text-3xl text-bold text-red-800">Monaco974</h1>
+    </>
+  );
+}
